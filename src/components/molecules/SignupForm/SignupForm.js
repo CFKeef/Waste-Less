f3f0fc94021f68ff6876dc57f91254545999a09b
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2020-12-17 15:55:25 -0500

    Started on Register component, need to create check box for terms and privacy agreement

---
+++ src/components/molecules/SignupForm/SignupForm.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import InputField from '../../atoms/InputField/InputField.js';
+import InputFieldLabel from '../../atoms/InputFieldLabel/InputFieldLabel.js';
+import GenericButton from "../../atoms/Button/Button.js"
+
+const SignupForm = (props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmedPassword, setConfirmedPassword] = useState("");
+    const [error, setError] = useState(false);
+
+    const handleSignup = () => {
+        console.log(email + " " + password)
+    }
+
+    const handleWrongInputs = () => {
+        if(error) return (
+            <View style={styles.error}>
+                <Text style={styles.errorText}>Something went wrong!</Text>
+                <Text style={styles.errorText}>Double check your information</Text>
+            </View>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.space}>
+                <View style={styles.labelSpace}>
+                    <InputFieldLabel text="Email Address"/>
+                </View>
+                <InputField
+                    placeholder={"Email Address"}
+                    value={email}
+                    setValue={setEmail}
+                    secure={false}
+                />
+            </View>
+            <View style={styles.space}>
+                <View style={styles.labelSpace}>
+                    <InputFieldLabel text="Password"/>
+                </View>
+                <InputField
+                    placeholder={"Password"}
+                    value={password}
+                    setValue={setPassword}
+                    secure={true}
+                />
+            </View>
+            <View style={styles.space}>
+                <View style={styles.labelSpace}>
+                    <InputFieldLabel text="Confirm Password"/>
+                </View>
+                <InputField
+                    placeholder={"Confirm Password"}
+                    value={confirmedPassword}
+                    setValue={setConfirmedPassword}
+                    secure={true}
+                />
+            </View>
+            <View style={styles.space}>
+                <GenericButton
+                    message={"Signup"}
+                    action={handleSignup}
+                    backgroundColor={"#592CBA"}
+                    textColor={"#FFD5CE"}
+                    color={"#592CBA"}
+                />
+            </View>
+            {handleWrongInputs()}
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 10,
+        marginBottom: 10
+    },
+    error: {
+        backgroundColor: "#fb6767",
+        alignSelf: "stretch",
+        marginTop: 20,
+        paddingTop:10,
+        paddingBottom: 10
+    },
+    errorText: {
+        fontSize: 14,
+        color: "white",
+        fontWeight: "600",
+        textAlign: "center"
+    },
+    space: {
+        marginTop: 20,
+    },
+    labelSpace: {
+        marginBottom: 5
+    }
+});
+
+export default SignupForm;
--- src/scenes/register/register.js
+++ src/scenes/register/register.js
@@ -1,29 +1,71 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import SignupForm from '../../components/molecules/SignupForm/SignupForm.js'
+import HyperLinkText from '../../components/atoms/HyperLinkText/HyperLinkText.js';
 
 const Register = ({ navigation }) => {
     return (
-        <View style={styles.container}>
-            <Text style={styles.logoHeader}>Waste Less</Text>
-            <Text> 
-                Already have an account? 
-                <Text onPress={() => navigation.navigate("Login")}> Login here!</Text>
-            </Text>
-        </View>
+        <SafeAreaView style={{flex: 1, backgroundColor: "#FBF6F2",}}>
+            <View
+                style={styles.container}
+            >   
+                <Text style={styles.logo}>Waste Less</Text>
+                <View style={styles.content}>
+                    <View style={styles.headerText}>
+                        <Text style={styles.title}>Signup</Text>
+                        <Text style={styles.subTitle}>Start managing your pantry the right way!</Text>
+                    </View>
+                    <SignupForm />
+                    <View style={styles.hyperLinkTextContainer}>
+                        <HyperLinkText
+                            message={"Already have an account? Sign in"}
+                            screen={"Login"}
+                            linkMessage={"Here"}
+                            navigation={navigation}
+                        />
+                    </View>
+                </View>
+            </View>
+        </SafeAreaView>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "#FBF6F2",
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        alignItems: "center",
+        justifyContent: "flex-start",
     },
-    logoHeader: {
-        fontSize: 30
+    hyperLinkTextContainer: {
+        alignSelf: "stretch",
+        marginTop: 10,
+    },
+    headerText: {
+        textAlign: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 28,
+        color: "#300076",
+        fontWeight: "700",
+    },
+    subTitle: {
+        fontSize: 16,
+        color: "#300076",
+        marginTop: 10,
+        marginBottom: 10,
+        flexWrap: "wrap"
+    },
+    logo: {
+        fontSize: 36,
+        color: "#300076",
+        fontWeight: "700"
+    },
+    content: {
+        marginTop: 100
+    },
+    extraSpace: {
+        marginTop: 20
     }
 });
-
 export default Register;
 
