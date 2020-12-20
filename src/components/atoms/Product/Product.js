7f9fa803fdfb24039cb11f5b74374c74119185bb
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2020-12-20 16:55:19 -0500

    Created cards and styled them. Need to work on creating new cards to really finish them

--- src/components/atoms/Product/Product.js
+++ src/components/atoms/Product/Product.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import {TouchableOpacity, View, Text, StyleSheet, Image} from 'react-native';
 
 const Product = (props) => {
     const handleExpirationText = () => {
@@ -11,24 +11,44 @@ const Product = (props) => {
 
         // Else green text
 
-        return props.expirationDate
+        return "Expired"
     };
+
+    const handleItemImage = (category) => {
+        switch (category) {
+            case "Meat":
+                return itemArr[0].imageLink
+        }
+    }
+
+    const handleExpirationStyling = () => {
+
+        return styles.expiredText
+    };
+
     return (
         <TouchableOpacity key={props.product.title + props.product.index} style={styles.container}>
-            <View style={styles.cardContainer}>
-                <View>
-                    <Text>{props.product.title}</Text>
+            <View style={styles.posContainer}>
+                <View style={styles.cardContainer}>
+                {console.log(props)}
+                    <View style={[styles.textContainer, styles.leftContainer]}>
+                        <Image 
+                            style={styles.categoryImg}
+                            source={require('./meat.png')}
+                        />
+                        <Text numberOfLines={1} style={[styles.otherText, styles.cardText, styles.leftText]}>{props.product.title}</Text>
+                    </View>
+                    <View style={[styles.textContainer, styles.leftContainer]}>
+                        <Text numberOfLines={1} style={[handleExpirationStyling(), styles.cardText, styles.leftText]}>{handleExpirationText()}</Text>
+                    </View>
                 </View>
-                <View>
-                    <Text>{handleExpirationText()}</Text>
-                </View>
-            </View>
-            <View>
-                <View>
-                    <Text>Count: {props.product.quantity + " " + props.product.unit}</Text>
-                </View>
-                <View>
-                    <Text>{props.product.location}</Text>
+                <View style={styles.cardContainer}>
+                    <View style={[styles.rightContainer, styles.textContainer]}>
+                        <Text numberOfLines={1} style={[styles.otherText, styles.cardText, styles.rightText]}>Count: {props.product.quantity + " " + props.product.unit}</Text>
+                    </View>
+                    <View style={[styles.textContainer, styles.rightContainer]}>
+                        <Text numberOfLines={1} style={[styles.otherText, styles.cardText, styles.rightText]}>{props.product.location}</Text>
+                    </View>
                 </View>
             </View>
         </TouchableOpacity>
@@ -46,8 +66,48 @@ const styles = StyleSheet.create({
         borderColor: "#e5e5e5",
         borderWidth: 1
     },
+    posContainer: {
+        justifyContent: "space-between",
+        flexDirection: "row",
+        width: "100%",
+        height: 80
+    },
     cardContainer: {
+        justifyContent: "space-between",
+        marginTop: 10,
+        marginBottom: 10,
+        width: "50%"
+    }, 
+    cardText: {
+        width: "100%",
+        fontSize: 14,
+        fontWeight: "700",
+    },
+    textContainer: {
         flexDirection: "row"
+    },
+    leftText: {
+        textAlign: "left",
+    },
+    rightText: {
+        textAlign: "right"
+    },
+    rightContainer: {
+        marginRight: 10
+    },
+    leftContainer: {
+        marginLeft: 10
+    },
+    categoryImg: {
+        height: 17,
+        width: 24,
+        marginRight: 10
+    },
+    otherText: {
+        color: "#300070"
+    },
+    expiredText: {
+        color: "#E276A0"
     }
 })
 
